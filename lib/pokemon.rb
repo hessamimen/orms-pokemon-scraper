64fb113157e13aed2f6666b3e5641dd0328c6ac2
Hessam Imen <hessam.imen@gmail.com>
2022-07-30 15:36:46 +1000

    commit tree 9bb4a30e381af779507b91d1a2ad2232dc99b8b5

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    
+    # def initialize(id:, name:, type:, db:)
+    #     @id, @name, @type, @db = id, name, type, db
+    # end
+    def initialize(name:, type:, id:, db:)
+        @name = name
+        @type = type
+        @id = id
+        @db = db
+      end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+    end
+
+    def self.find(num, db)
+        sql = <<-SQL
+            SELECT * FROM pokemon
+            WHERE id = ?
+        SQL
+        info = db.execute(sql, num).flatten 
+        Pokemon.new(id: info[0], name: info[1], type: info[2], db: db) 
+    end
+
 end
